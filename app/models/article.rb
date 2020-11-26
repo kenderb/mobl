5ba6bc9a3773864df7dccf5fb21696e7b6026f4e
kenderb <kenderboliar@gmail.com>
2020-11-26 08:30:26 -0500

    Add validations to the article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 6, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 300 }
 end
