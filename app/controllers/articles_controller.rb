4ff0d524f8cfa994bbba644648af12a47fac571c
kenderb <kenderbolivar@gmail.com>
2020-12-08 20:39:58 -0500

    Add the update method for the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,12 +14,28 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created successfully."
+      flash[:notice] = 'Article was created successfully.'
       redirect_to @article
     else
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
