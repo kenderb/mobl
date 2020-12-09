36503dacb018b291c20297397080697a5e1bdb29
kenderb <kenderbolivar@gmail.com>
2020-12-08 20:10:40 -0500

    Add flash and error messages to the App

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
+      flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
       render 'new'
