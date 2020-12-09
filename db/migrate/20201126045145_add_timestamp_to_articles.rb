942835fb8e15273fdff6d50d2a32799185b2dccb
kenderb <kenderbolivar@gmail.com>
2020-12-08 22:59:12 -0500

    fix datetime error

--- db/migrate/20201126045145_add_timestamp_to_articles.rb
+++ db/migrate/20201126045145_add_timestamp_to_articles.rb
@@ -1,6 +1,6 @@
 class AddTimestampToArticles < ActiveRecord::Migration[6.0]
   def change
-    add_column :articles, :created_at, :datetimme
-    add_column :articles, :updated_at, :datetimme
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
   end
 end
